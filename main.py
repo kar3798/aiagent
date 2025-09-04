d12af7cfdf777a12fa16187bd5e895726cb3954f
Kartikeya <kay54@drexel.edu>
2025-09-04 18:15:11 -0400

    Taking prompt at arguement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,8 +9,13 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    if len(sys.argv) < 2:
+        print("Please enter a prompt!")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model='gemini-2.0-flash-001', contents=prompt
     )
 
     print(response.text)
