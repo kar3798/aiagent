73aa3efffd461931603188f92c7f7f6fa271b1dd
Kartikeya <kay54@drexel.edu>
2025-09-04 18:28:38 -0400

    added verbose flag

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def main():
     if len(sys.argv) < 2:
         print("Please enter a prompt!")
         sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
     prompt = sys.argv[1]
 
     messages = [
@@ -27,7 +30,10 @@ def main():
     if response is None or response.usage_metadata is None:
         print("response is malformed")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 main()
