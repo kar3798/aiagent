dba4f9ba7cd0ccdddc6e378caa33310787f68f15
Kartikeya <kay54@drexel.edu>
2025-09-04 18:09:03 -0400

    Hardcoded Prompt

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from aiagent!")
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
