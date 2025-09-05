baed9f805cc1ca6748137e529ad53b4b0b6b620f
Kartikeya <kay54@drexel.edu>
2025-09-04 20:07:33 -0400

    write content to file

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: {file_path} is not in the working directory'
+
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.isdir(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f'Could not create the parent directory: {parent_dir} = {e}'
+    try:
+        with open(abs_file_path, 'w') as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f'Failed to write to {file_path}, {e}'
--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
     working_dir = "calculator"
+    # Tests for get file info
     # root_contents =  get_files_info(working_dir)
     # print(root_contents)
     # pkg_contents = get_files_info(working_dir, "pkg")
@@ -11,9 +13,17 @@ def main():
     # print(pkg_contents)
     # pkg_contents = get_files_info(working_dir, "../")
     # print(pkg_contents)
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
-    print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+    # Tests for get file content
+    # print(get_file_content("calculator", "main.py"))
+    # print(get_file_content("calculator", "pkg/calculator.py"))
+    # print(get_file_content("calculator", "/bin/cat"))
+    # print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+    # Tests to write file content
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum\n"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet\n"))
+    print(write_file("calculator", "pkg2/morelorem.txt", "lorem ipsum dolor sit amet\n"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed\n"))
 
 main()
